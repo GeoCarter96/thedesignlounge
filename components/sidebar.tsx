074d3a722549997c1d7f09900084efb770a82894
GeoCarter96 <geolcarter21@gmail.com>
2026-01-27 00:09:48 -0500

    add

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -48,7 +48,7 @@ export default function Sidebar() {
    {/* Scrollable Container Wrapper */}
 <div className="flex-1 overflow-y-auto pr-4 custom-scrollbar">
   <nav className="flex flex-col gap-10">
-    {['The Lobby', 'Freebies', 'Testimonials',  'Courses', 'Ebooks','Flyers','Planners','PrivateLounge', 'Lounge Menu','Owner','CustomWork', 'Contact'].map((item, i) => (
+    {['The Lobby', 'Freebies', 'Testimonials',  'Courses', 'Ebooks','Flyers','Planners','PrivateLounge', 'LoungeMenu','Owner','CustomWork', 'Contact'].map((item, i) => (
       <motion.div
         key={item}
         initial={{ opacity: 0, x: 20 }}
@@ -56,7 +56,7 @@ export default function Sidebar() {
         transition={{ delay: 0.1 * i }}
       >
         <Link 
-          href={item === 'The Lobby' ? '/'  : item === 'Lounge Menu' 
+          href={item === 'The Lobby' ? '/'  : item === 'LoungeMenu' 
             ? '/theloungemenu' : `/${item.toLowerCase().replace(/\s+/g, '-')}`}
           className="relative block text-4xl font-extralight tracking-tighter group overflow-hidden"
           onClick={() => setIsOpen(false)}
